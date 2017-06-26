85829f65639e3b01c883fefa106407213e2ba036
jim <james.schmitz@nyu.edu>
2017-06-26 17:38:58 +0000

    move app stuff

--- index.js
+++ index.js
@@ -1,45 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
 const debug = require('debug')('itp');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const nedbstore = require('nedb-session-store')(session);
 
-const user = require('_/user');
+const app = require('_/app');
 
-// setup express
-var app = express();
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.set('view engine', 'ejs');
-app.set('views', 'templates/');
-app.use(express.static('static'));
-app.use(cookieParser());
-app.use(
-  session({
-    secret: 'secret',
-    cookie: {
-      maxAge: 5 * 60 * 1000
-    },
-    store: new nedbstore({
-      filename: 'sessions.db'
-    }),
-    resave: true,
-    saveUninitialized: true
-  })
-);
-
-// *** routes ***
-app.get('/filter', user.initFilter);
-app.get('/filter/:cc/:cat/:rec', user.filter);
-app.post('/drawings', user.drawings);
-app.post('/flag', user.flag);
-
-// finally, launch the server
 const PORT = 8080;
 app.listen(PORT, () => {
   debug(`Server is running on port ${PORT}`);
 });
-
-module.exports = app;
