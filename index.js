d8c3bf9580127b8b73999d6e83f5342819726dd0
jim <james.schmitz@nyu.edu>
2017-06-27 01:23:07 +0000

    prod environment

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const debug = require('debug')('itp');
 
+const config = require('_/config')
 const app = require('_/app');
 
-const PORT = 8080;
+const PORT = config.portNumber;
 app.listen(PORT, () => {
   debug(`Server is running on port ${PORT}`);
 });
