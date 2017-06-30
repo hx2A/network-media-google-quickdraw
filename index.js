c7c434284280b608eae803f6e660cbc97a38efee
jim <james.schmitz@nyu.edu>
2017-06-30 17:23:06 +0000

    use ssl

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const debug = require('debug')('itp');
+var https = require('https');
 
 const config = require('_/config')
-const app = require('_/app');
+const server = require('_/server');
 
 const PORT = config.portNumber;
-app.listen(PORT, () => {
+server.app.listen(PORT, () => {
   debug(`Server is running on port ${PORT}`);
 });
+
+if (config.useSSL) {
+  var httpsServer = https.createServer(server.ssl.credentials, server.app);
+  httpsServer.listen(443);
+}
